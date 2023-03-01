8fda13535e10812c4be1310e5e58df47929364c8
おもちくん <sei.t11@icloud.com>
2023-03-02 06:01:15 +0900

    add hotkey.js

---
+++ hotkey.js
@@ -0,0 +1,28 @@
+class Hotkeys{
+    static modifier = {
+        ctrl:"",
+        shift:"",
+        alt:"",
+        cmd:""
+    }
+
+    constructor(){
+
+    }
+
+    add(){
+
+    }
+
+    remove(){
+
+    }
+
+    start(){
+
+    }
+    
+    stop(){
+
+    }
+}
