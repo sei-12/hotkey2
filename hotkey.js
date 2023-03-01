036de1a8e6e9b5ad967a488676b725d4ba0cb8ab
おもちくん <sei.t11@icloud.com>
2023-03-02 06:49:44 +0900

    upadate hokeyjs

--- hotkey.js
+++ hotkey.js
@@ -9,22 +9,66 @@ class Hotkeys{
 }
 
 class Modifier{
-    static Ctrl  = ""
-    static Shift = ""
+    static Ctrl  = "Control"
+    static Shift = "Shift"
     static Alt   = ""
-    static Cmd   = ""
+    static Cmd   = "Meta"
 }
 
-class Hotkey{
-    constructor(node,modifiers,char,func){
+const Hotkey = function(node,modifiers,char,func){
 
+    const setModikyIsDown = () => {
+        let modikeyIsDown = []
+        this.modifiers.forEach(modifier => {
+            modikeyIsDown[modifier] = false
+        })
+        return modikeyIsDown
     }
-    
-    start(){
 
+    const handleNodeKeydown = (e) => {
+        if(this.modikeyIsDown.every(x => x) == false){
+            return
+        }
+
+        if(this.modifiers.includes(Modifier.Shift)){
+            // シフトの場合の処理を反映
+            // a -> A
+        }
+        
+        if(e.key == this.char){
+            this.func()
+        }
     }
 
-    stop(){
-
+    const handleDocumentKeydown = (e) => {
+        if(this.modikeyIsDown[e.key] != undefined){
+            this.modikeyIsDown[e.key] = true
+        }
     }
+
+    const handleDocumentKeyup = (e) => {
+        if(this.modikeyIsDown[e.key] != undefined){
+            this.modikeyIsDown[e.key] = false
+        }
+    }
+
+    this.start = function(){
+        this.node.addEventListener("keydown",handleNodeKeydown)
+        document.addEventListener("keydown",handleDocumentKeydown)
+        document.addEventListener("keyup",handleDocumentKeyup)
+    }
+
+    this.stop = function(){
+        this.node.removeEventListener("keydown",handleNodeKeydown)
+        document.removeEventListener("keydown",handleDocumentKeydown)
+        document.removeEventListener("keyup",handleDocumentKeyup)
+    }
+
+    this.node = node
+    this.modifiers = modifiers
+    this.char = char
+    this.func = func
+
+    this.modikeyIsDown = setModikyIsDown()
 }
+
