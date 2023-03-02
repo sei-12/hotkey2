d63e184d00f81d5cd0f2d8911799ce8336ab86f6
おもちくん <sei.t11@icloud.com>
2023-03-02 17:36:27 +0900

    update testcode

--- test-code/test.js
+++ test-code/test.js
@@ -4,7 +4,8 @@ const inputText = document.getElementById("input-text")
 let hotkeys = [
     new Hotkey(document,[Modifier.Cmd],"a",()=>{console.log("document Cmd a")}),
     new Hotkey(document,[Modifier.Cmd,Modifier.Ctrl],"v",()=>{console.log("document Cmd Ctrl v")}),
-    new Hotkey(inputText,[Modifier.Cmd],"u",()=>{console.log("inputText Cmd y")}),
+    new Hotkey(inputText,[Modifier.Cmd],"u",()=>{console.log("inputText Cmd u")}),
 ]
 
 hotkeys.forEach( hotkey => hotkey.start())
+// window.addEventListener("keydown",e=>console.log(e.key))
